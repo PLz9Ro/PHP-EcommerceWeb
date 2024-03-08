691832b0561cb6c39061d5e9d9cf376eddcb2a13
PhucLam <lamthanhphucit@gmail.com>
2024-03-08 18:33:15 +0700

    AdminMiddleware

--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -13,14 +13,18 @@ use App\Http\Controllers\admin\Authcontroller;
 |
 */
 //admin
+
+Route::group(['middleware'=>'admin'],function(){
+    Route::get('admin/dashboard', function () {
+        return view('admin.dashboard');
+    });
+});
 Route::get('admin',[AuthController::class,'login_admin']);
 Route::post('admin',[AuthController::class,'auth_login_admin']);
 Route::get('admin/logout',[AuthController::class,'logout_admin']);
 
 
-Route::get('admin/dashboard', function () {
-    return view('admin.dashboard');
-});
+
 Route::get('/', function () {
     return view('welcome');
 });
